1316ea391484784a2482a10d841d50d1a183fed7
Blaine Schmeisser <blainesch@gmail.com>
2016-05-02 15:49:55 -0700

    Give back title and subtitle as results.

--- calculator.js
+++ calculator.js
@@ -2,8 +2,8 @@ const equation = process.argv.slice(-1)[0]
 
 try {
   console.log(JSON.stringify([
-    {value: eval(equation)},
-    {value: eval(equation)},
-    {value: eval(equation)},
+    {title: eval(equation), subtitle: 'Select item to copy the value to the clipboard.'},
+    {title: eval(equation), subtitle: 'Select item to copy the value to the clipboard.'},
+    {title: eval(equation), subtitle: 'Select item to copy the value to the clipboard.'},
   ]))
 } catch (e) {}
