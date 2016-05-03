ca2964f4c16caa8aac0ab84b8da2c4730012d45a
Blaine Schmeisser <blainesch@gmail.com>
2016-05-02 21:08:54 -0700

    Update to only show one result and give invalid expression warning.

--- calculator.js
+++ calculator.js
@@ -3,7 +3,9 @@ const equation = process.argv.slice(-1)[0]
 try {
   console.log(JSON.stringify([
     {title: eval(equation), subtitle: 'Select item to copy the value to the clipboard.'},
-    {title: eval(equation), subtitle: 'Select item to copy the value to the clipboard.'},
-    {title: eval(equation), subtitle: 'Select item to copy the value to the clipboard.'},
   ]))
-} catch (e) {}
+} catch (e) {
+  console.log(JSON.stringify([
+    {title: '...', subtitle: 'Please enter a valid expression.'},
+  ]))
+}
