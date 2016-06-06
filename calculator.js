5e6c1cc4edb3b569aa0c85116ba0940c9a5dc6db
Blaine Schmeisser <blainesch@gmail.com>
2016-06-06 09:04:28 -0700

    Add formatting to the answers.
    The copy/paste does not contain commas.

--- calculator.js
+++ calculator.js
@@ -2,22 +2,22 @@
 
 const equation = process.argv.slice(-1)[0]
 const mathjs = require('mathjs')
+const numeral = require('numeral')
 
 try {
-  let answer = mathjs.eval(equation)
-  let value = answer
-  if (typeof answer === "object") {
-    value = answer.value
-    answer = answer.toString()
-  }
+  let answer = mathjs.eval(equation).toString()
+  let title = answer.replace(/\d+/, (v) => {
+    return numeral(v).format('0,0')
+  })
   console.log(JSON.stringify([
     {
-      title: answer,
+      title: title,
       subtitle: 'Select item to copy the value to the clipboard.',
-      value: value,
+      value: answer,
     },
   ]))
 } catch (e) {
+  console.log(e)
   console.log(JSON.stringify([
     {
       title: '...',
